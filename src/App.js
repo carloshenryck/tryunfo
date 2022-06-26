b63e06d3835b25a9b5d013d7f0e986853b96f2fd
Henryck <ryck302@gmail.com>
2022-06-26 13:47:04 -0300

    requisito 0e

--- src/App.js
+++ src/App.js
@@ -1,36 +1,37 @@
 import React from 'react';
 import Form from './components/Form';
+import Card from './components/Card';
 
 class App extends React.Component {
   render() {
-    let cardName;
-    let cardDescription;
-    let cardAttr1;
-    let cardAttr2;
-    let cardAttr3;
-    let cardImage;
-    let cardRare;
-    let cardTrunfo;
-    let hasTrunfo;
-    let isSaveButtonDisabled;
-    let onInputChange;
-    let onSaveButtonClick;
-
     return (
-      <Form
-        cardName={ cardName }
-        cardDescription={ cardDescription }
-        cardAttr1={ cardAttr1 }
-        cardAttr2={ cardAttr2 }
-        cardAttr3={ cardAttr3 }
-        cardImage={ cardImage }
-        cardRare={ cardRare }
-        cardTrunfo={ cardTrunfo }
-        hasTrunfo={ hasTrunfo }
-        isSaveButtonDisabled={ isSaveButtonDisabled }
-        onInputChange={ onInputChange }
-        onSaveButtonClick={ onSaveButtonClick }
-      />
+      <>
+        <Form
+          cardName=""
+          cardDescription=""
+          cardAttr1=""
+          cardAttr2=""
+          cardAttr3=""
+          cardImage=""
+          cardRare=""
+          cardTrunfo={ false }
+          hasTrunfo={ false }
+          isSaveButtonDisabled={ false }
+          onInputChange={ () => {} }
+          onSaveButtonClick={ () => {} }
+        />
+
+        <Card
+          cardName=""
+          cardDescription=""
+          cardAttr1=""
+          cardAttr2=""
+          cardAttr3=""
+          cardImage=""
+          cardRare=""
+          cardTrunfo={ false }
+        />
+      </>
     );
   }
 }
