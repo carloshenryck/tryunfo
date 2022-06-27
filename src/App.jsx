d1f969d2a6698c49593df0ff475a8ab7a2303929
Henryck <ryck302@gmail.com>
2022-06-27 00:43:06 -0300

    requisito 05

--- src/App.jsx
+++ src/App.jsx
@@ -19,13 +19,42 @@ class App extends React.Component {
     };
   }
 
+  verifyAttrs = () => {
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const attrs = [parseInt(cardAttr1, 10), parseInt(cardAttr2, 10),
+      parseInt(cardAttr3, 10)];
+    const max = 90;
+    const min = 0;
+    const maxSum = 210;
+
+    for (let i = 0; i < attrs.length; i += 1) {
+      if (attrs[i] > max || attrs[i] < min || Number.isNaN(attrs[i])) {
+        return false;
+      }
+    }
+
+    const sum = attrs.reduce((acc, i) => acc + i, 0);
+    return sum <= maxSum;
+  }
+
+  validateButton = () => {
+    const { cardName, cardDescription, cardImage } = this.state;
+
+    if (cardName !== '' && cardDescription !== ''
+    && cardImage !== '' && this.verifyAttrs()) {
+      this.setState({ isSaveButtonDisabled: false });
+    } else {
+      this.setState({ isSaveButtonDisabled: true });
+    }
+  }
+
   onInputChange = ({ target }) => {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
 
     this.setState({
       [name]: value,
-    });
+    }, this.validateButton);
   };
 
   onSaveButtonClick = () => {};
